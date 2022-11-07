ac917e6e26ca0822fefee8eb7d183c89fe0f5a51
QkoSad <malakrapon@gmail.com>
2022-11-07 19:37:10 +0200

    moving online

---
+++ src/context/FeedbackContext.js
@@ -0,0 +1,80 @@
+import { createContext, useState, useEffect } from "react";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [isloading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
+    const data = await response.json();
+    setFeedback(data);
+    setIsLoading(false);
+  };
+
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch("/feedback", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newFeedback),
+    });
+    const data = await response.json();
+    setFeedback([data, ...feedback]);
+  };
+
+  const deleteFeedback = async (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      await fetch(`/feedback/${id}`, { method: "DELETE" });
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updItem),
+    });
+
+    const data = await response.json();
+    setFeedback(feedback.map((item) => (item.id === id ? data : item)));
+    setFeedbackEdit({ item: {}, edit: false });
+  };
+
+  //set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
+  return (
+    <FeedbackContext.Provider
+      value={{
+        feedback,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        feedbackEdit,
+        updateFeedback,
+        isloading,
+      }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+
+export default FeedbackContext;
